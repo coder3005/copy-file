bbccf9b717c5eed6d5e16ec8d8e84fc7bca0e92b
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-11 10:45:51 +0530

    Create copyFile1.java

---
+++ copyFile1.java
@@ -0,0 +1,17 @@
+/* copy one file data to another */
+import java.io.*;
+class copyFile
+{
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream r=new FileInputStream("C:\\Users\\Abhi Gupta\\downloads\\UP.txt");
+        FileOutputStream w=new FileOutputStream("C:\\Users\\Abhi Gupta\\downloads\\OP.txt");
+
+        int i;
+        while((i=r.read())!=-1)
+        {
+            w.write((char)i);
+        }
+        System.out.println("Data Copied Successfully");
+    }
+}
